abc19b12fa6fb8492e8917088ced4d64e53e925e
Blikq <blikqmain@gmail.com>
2024-06-18 11:22:53 +0100

    update

--- frontend/index.js
+++ frontend/index.js
@@ -16,11 +16,11 @@ app.use(express.urlencoded({
 // Enable CORS for all routes
 // app.use(cors());
 
-const port = process.env.PORT || 3000;
-const environment = process.env.ENVIRONMENT || 'sandbox';
-const client_id = process.env.CLIENT_ID;
-const client_secret = process.env.CLIENT_SECRET;
-const endpoint_url = environment === 'sandbox' ? 'https://api-m.sandbox.paypal.com' : 'https://api-m.paypal.com';
+// const port = process.env.PORT || 3000;
+// const environment = process.env.ENVIRONMENT || 'sandbox';
+// const client_id = process.env.CLIENT_ID;
+// const client_secret = process.env.CLIENT_SECRET;
+// const endpoint_url = environment === 'sandbox' ? 'https://api-m.sandbox.paypal.com' : 'https://api-m.paypal.com';
 
 app.post('/create_order', (req, res) => {
     
@@ -49,5 +49,5 @@ app.get('/script.js', (req, res) => {
 
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`)
+    console.log(`Server listening at http://localhost:3000`)
 })
