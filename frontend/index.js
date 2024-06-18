855c868e248e44fb282b65e0eb41b59d74c17866
Blikq <blikqmain@gmail.com>
2024-06-18 09:02:53 +0100

    signin and login init

---
+++ frontend/index.js
@@ -0,0 +1,53 @@
+// import fetch from 'node-fetch';
+// import cors from 'cors';
+import express from "express";
+
+
+import 'dotenv/config';
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+// Enable CORS for all routes
+// app.use(cors());
+
+const port = process.env.PORT || 3000;
+const environment = process.env.ENVIRONMENT || 'sandbox';
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+const endpoint_url = environment === 'sandbox' ? 'https://api-m.sandbox.paypal.com' : 'https://api-m.paypal.com';
+
+app.post('/create_order', (req, res) => {
+    
+});
+
+
+
+// Helper / Utility functions
+
+//Servers the index.html file
+app.get('/signup', (req, res) => {
+    res.sendFile(process.cwd() + '/views/signUp.html');
+});
+app.get('/login', (req, res) => {
+    res.sendFile(process.cwd() + '/views/login.html');
+});
+
+//Servers the style.css file
+app.get('/style.css', (req, res) => {
+    res.sendFile(process.cwd() + '/style.css');
+});
+//Servers the script.js file
+app.get('/script.js', (req, res) => {
+    res.sendFile(process.cwd() + '/script.js');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`)
+})
---
+++ frontend/public/sidebar-func.js
@@ -0,0 +1,4 @@
+$('html').click(function() {
+    $('.sidebar').hide(); 
+ });
+ 
